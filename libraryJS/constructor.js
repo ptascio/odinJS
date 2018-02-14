11cac7e177567e81e2049326d0a6521d4f0e3bfe
Peter Tascio <peter.tascio@gmail.com>
2018-02-13 20:26:02 -0500

    Delete works

--- libraryJS/constructor.js
+++ libraryJS/constructor.js
@@ -1,3 +1,9 @@
+//if last id is deleted, it must be stored so that next id picks up where
+//left off. if lastId < veryLastId, don't change it (e.g., delete 5 then 4, it status
+//at 5) so that means we delete the last one...
+//
+
+
 let myLib = [{"author": "Bob",
 "title": "Bob is great!",
 "pages": "133",
@@ -9,6 +15,7 @@ let bookForm = document.getElementById("bookForm");
 let revealBtn = document.getElementById("addBookBtn");
 let bookSubmissionBtn = document.getElementById("submitBk");
 let deletedId;
+let latestDltdId;
 let newSubmission = {
   "author": "",
   "title": "",
@@ -25,17 +32,18 @@ function addBookToLib(book){
 }
 
 function render(){
-  let newRow = createEl("tr");
-  let rmvBtn = createEditBtn("Remove");
-  newRow.appendChild(rmvBtn);
+  let newRow;
+  let rmvBtn;
   for(var i = 0; i < myLib.length; i++){
+  newRow = createEl("tr");
+  rmvBtn = createEditBtn("Remove");
+    newRow.appendChild(rmvBtn);
     let bk = myLib[i];
-
+    newRow.setAttribute("id", bk["id"]);
     for (var key in bk){
         if (bk.hasOwnProperty(key)){
       let newTd = createEl("td");
-
-      if (bk[key]){
+      if (bk[key] !== "id"){
         newTd.innerText = bk[key];
       }
       newRow.appendChild(newTd);
@@ -46,9 +54,14 @@ function render(){
 }
 
 function addId(){
-  let ltsEntryId = myLib[myLib.length - 1]["id"];
-  ltsEntryId+=1;
-  return ltsEntryId;
+
+  let ltsEntryId;
+  if (latestDltdId !== undefined){
+    ltsEntryId = parseInt(latestDltdId);
+  }else{  ltsEntryId = myLib[myLib.length - 1]["id"];}
+  let newId = ltsEntryId+1;
+  latestDltdId = undefined;
+  return newId;
 }
 
 function renderLatest(){
@@ -58,6 +71,7 @@ function renderLatest(){
   newRow.appendChild(rmvBtn);
   let newTd;
   for (var key in newEntry){
+    newRow.setAttribute("id", newEntry["id"]);
     if (newEntry.hasOwnProperty(key)){
       newTd = createEl("td");
       newTd.innerText = newEntry[key];
@@ -70,6 +84,11 @@ function renderLatest(){
 function createEditBtn(type){
   let editTd = createEl("td");
   editTd.innerText = `${type}`;
+  editTd.setAttribute("style", "cursor: pointer;");
+  let evntListnr;
+  if (type === "Remove"){
+    editTd.addEventListener("click", getIdOfBookToDelete);
+  }
   return editTd;
 }
 
@@ -112,6 +131,7 @@ function getSubmission(e){
  e.preventDefault();
  newSubmission["id"] = addId();
  myLib.push(newSubmission);
+ resetSubmission();
  renderLatest();
  clearFields();
 }
@@ -124,15 +144,54 @@ function clearFields(){
   resetBtns();
 }
 
+function resetSubmission(){
+  newSubmission = {
+    "author": "",
+    "title": "",
+    "pages": "",
+    "read": "",
+    "id": null
+  };
+}
+
 function resetBtns(){
   for(var d = 0; d < rdioBtns.length; d++){
     rdioBtns[d].checked = false;
   }
 }
 
-function deleteBook(){
- debugger
+function getIdOfBookToDelete(){
+ deletedId = this.parentNode.id;
+ if (latestDltdId === undefined && deletedId >= myLib[myLib.length-1]["id"]){
+   latestDltdId = deletedId;
+ }
+ deleteBookFromLib(deletedId);
 }
+
+function deleteBookFromLib(id){
+  let bkIdx;
+  for(var x = 0; x < myLib.length; x++){
+    if(myLib[x]["id"] === parseInt(id)){
+      removeBook(x);
+    }
+  }
+}
+
+function removeBook(idx){
+  let firstHalf = myLib.slice(0, idx);
+  let secondHalf = myLib.slice(idx + 1);
+  myLib = firstHalf.concat(secondHalf);
+  deleteBooksFromDOM();
+  render();
+}
+
+function deleteBooksFromDOM(){
+  while(bookList.firstChild){
+    bookList.removeChild(bookList.firstChild);
+  }
+}
+
+
 
 revealBtn.addEventListener("click", revealForm);
 bookSubmissionBtn.addEventListener("click", getSubmission);
